5ad81cce6b12d72a7d94a2a6827b865678428ec6
Robert Steiner <robert@flower.ai>
2024-05-16 16:10:33 +0200

    wip
    Signed-off-by: Robert Steiner <robert@flower.ai>

--- dev/build-docker-image-matrix.py
+++ dev/build-docker-image-matrix.py
@@ -1,12 +1,13 @@
 import argparse
 import json
+from typing import Any, Callable
 
-
+ALPINE_VERSION = "alpine3.19"
 BASE_OSS = [
     {"tag": "ubuntu22.04", "version": "22.04", "file_dir": "src/docker/base/ubuntu"},
     {
-        "tag": "alpine3.19",
-        "version": "alpine3.19",
+        "tag": ALPINE_VERSION,
+        "version": ALPINE_VERSION,
         "file_dir": "src/docker/base/alpine",
     },
 ]
@@ -15,7 +16,9 @@ LATEST_SUPPORTED_PYTHON_VERSIONS = "3.11"
 SUPPORTED_PYTHON_VERSIONS = ["3.8", "3.9", "3.10", LATEST_SUPPORTED_PYTHON_VERSIONS]
 
 
-def generate_base_matrix_item(flwr_version, python_version, os):
+def generate_base_matrix_item(
+    flwr_version: str, python_version: str, os: dict[str, str]
+) -> dict[str, Any]:
     return {
         "os": os,
         "python_version": python_version,
@@ -24,7 +27,9 @@ def generate_base_matrix_item(flwr_version, python_version, os):
     }
 
 
-def generate_base_image_matrix(flwr_version, python_versions, oss):
+def generate_base_image_matrix(
+    flwr_version: str, python_versions: list[str], oss: list[dict[str, str]]
+) -> list[dict[str, Any]]:
     return [
         generate_base_matrix_item(flwr_version, py, os)
         for os in oss
@@ -32,48 +37,59 @@ def generate_base_image_matrix(flwr_version, python_versions, oss):
     ]
 
 
-def generate_matrix_item(file_dir, base_image, conditional_tags):
+def generate_binary_matrix_item(
+    namespace_repository: str,
+    file_dir: str,
+    base_image: str,
+    conditional_tags: None | Callable,
+) -> dict[str, Any]:
     tags = [base_image["tag"]]
     if conditional_tags is not None:
-        tags + (conditional_tags(base_image) or [])
+        tags += conditional_tags(base_image) or []
 
     return {
+        "namespace_repository": namespace_repository,
         "file_dir": file_dir,
         "base_image": base_image["tag"],
         "tags": "\n".join(tags),
     }
 
 
-def generate_matrix(
-    file_dir,
-    base_images,
-    conditional_tags=None,
-    filter=None,
-):
-    filter = filter or (lambda image: True)
+def generate_binary_matrix(
+    namespace_repository: str,
+    file_dir: str,
+    base_images: str,
+    conditional_tags: None | Callable = None,
+    filter: None | Callable = None,
+) -> list[dict[str, Any]]:
+    filter = filter or (lambda _: True)
 
     return [
-        generate_matrix_item(file_dir, image, conditional_tags)
+        generate_binary_matrix_item(
+            namespace_repository, file_dir, image, conditional_tags
+        )
         for image in base_images
         if filter(image)
     ]
 
 
-def is_latest_python_alpine_image(image):
+def is_latest_python_alpine_image(image: dict[str, Any]) -> bool:
     return (
-        image["os"]["version"] == "alpine3.19"
+        image["os"]["version"] == ALPINE_VERSION
         and image["python_version"] == LATEST_SUPPORTED_PYTHON_VERSIONS
     )
 
 
-arg_parser = argparse.ArgumentParser(description="Generate GitHub Docker image matrix")
+arg_parser = argparse.ArgumentParser(
+    description="Generate Github Docker workflow matrix"
+)
 arg_parser.add_argument("--flwr-version", type=str, required=True)
 args = arg_parser.parse_args()
 
 flwr_version = args.flwr_version
 
 
-def on_latest_python_alpine_image(image):
+def on_latest_python_alpine_image(image: dict[str, Any]) -> None | list[str]:
     if is_latest_python_alpine_image(image):
         return [flwr_version]
     else:
@@ -86,32 +102,28 @@ base_images = generate_base_image_matrix(
     BASE_OSS,
 )
 
-print(
-    json.dumps(
-        {
-            "base": {"images": base_images},
-            "superlink": {
-                "images": generate_matrix(
-                    "src/docker/superlink",
-                    base_images,
-                    on_latest_python_alpine_image,
-                    is_latest_python_alpine_image,
-                )
-            },
-            "supernode": {
-                "images": generate_matrix(
-                    "src/docker/supernode",
-                    base_images,
-                    on_latest_python_alpine_image,
-                )
-            },
-            "serverapp": {
-                "images": generate_matrix(
-                    "src/docker/serverapp",
-                    base_images,
-                    on_latest_python_alpine_image,
-                )
-            },
-        }
+binary_images = (
+    generate_binary_matrix(
+        "flwr/superlink",
+        "src/docker/superlink",
+        base_images,
+        on_latest_python_alpine_image,
+        is_latest_python_alpine_image,
     )
+    + generate_binary_matrix(
+        "flwr/supernode",
+        "src/docker/supernode",
+        base_images,
+        on_latest_python_alpine_image,
+    )
+    + generate_binary_matrix(
+        "flwr/serverapp",
+        "src/docker/serverapp",
+        base_images,
+        on_latest_python_alpine_image,
+    )
+)
+
+print(
+    json.dumps({"base": {"images": base_images}, "binary": {"images": binary_images}})
 )
